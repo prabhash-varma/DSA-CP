bd0ecd3ff72185cbd66faab12ebfdd8067376a64
Prabhash Varma <varmaprabhash@gmail.com>
2021-12-26 12:03:50 +0530

    update

---
+++ Graphs/Graph1.cpp
@@ -0,0 +1,33 @@
+
+
+/*
+    Graph/ Trees
+
+        -Representation
+        -Traversal
+        -Algorithms
+
+
+            Representation
+                -Matrix
+                -List
+                    -Arrays of vectors
+                    -vectors of vectors
+
+            
+
+            Traversal
+                -DFS
+                -BFS
+
+
+            Algorithms
+                -Dijkstra
+                -Bellman-Ford
+                -Disjoint set union
+                -Minimum spanning tree
+                -Directed Acyclic Graph
+                -Topological Sort
+
+
+*/
---
+++ Graphs/Graph2_Terminologies.cpp
@@ -0,0 +1,12 @@
+/*
+    -Vertices/Edges
+    -Undirected/Directed Graphs
+    -Connected Components
+    -Strongly Connected Components
+    -Height & Depth in a Tree
+    -Root/Child/Parents/Leaf Nodes in a tree
+
+
+    
+
+*/
